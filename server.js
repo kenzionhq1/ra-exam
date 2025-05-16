b0cb4607bdec2c1d513168d8e012c04f0cf56f78
OGUNFOWORA TWINS <OGUNFOWORATWINS@OGUNFOWORA-TWINS-macBOOK-AIR.local>
2025-05-17 00:57:03 +0100

    Fix Render port and Mongo options

--- server.js
+++ server.js
@@ -17,10 +17,8 @@ app.use('/api', examRoutes);
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('MongoDB connected');
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}).catch(err => console.error('Mongo error:', err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  }).catch(err => console.error('MongoDB error:', err));
