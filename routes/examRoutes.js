157388455fb1b74f78797e435caca5481a141627
OGUNFOWORA TWINS <OGUNFOWORATWINS@OGUNFOWORA-TWINS-macBOOK-AIR.local>
2025-05-19 23:09:56 +0100

    Fix duplicate import of Result in examController

--- routes/examRoutes.js
+++ routes/examRoutes.js
@@ -12,8 +12,6 @@ router.get('/questions', getQuestions);
 router.post('/submit-exam', submitExam);
 router.get('/results', getAllResults);
 router.get('/result/:id', getResultById);
-
-// NEW:
 router.post('/check-submitted', checkAlreadySubmitted);
 
 export default router;
